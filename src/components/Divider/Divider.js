60a327aafdd27fcafd4a993d9cc8a56c0f46e58b
bobbyinfj <RobertNakano@gmail.com>
2020-11-25 03:35:59 +0000

    divider lines thickness and strength

--- src/components/Divider/Divider.js
+++ src/components/Divider/Divider.js
@@ -5,31 +5,19 @@ import './divider.css'
 /**
  * Primary UI component for user interaction
  */
-export const Divider = ({ primary, color, strength, thick, label, ...props }) => {
-  const mode = primary
-    ? 'storybook-Divider--primary'
-    : 'storybook-Divider--secondary'
+export const Divider = ({ strength, thick, ...props }) => {
 
   return (
     <hr
-      className={['storybook-divider', `storybook-divider--${thick}`, `storybook-divider--${strength}`, mode].join(
+      className={['storybook-divider', `storybook-divider--${thick}`, `storybook-divider--${strength}`].join(
         ' '
       )}
-      style = {color && {color}}
       {...props}
       />
   )
 }
 
 Divider.propTypes = {
-  /**
-   * Is this the principal call to action on the page?
-   */
-  primary: PropTypes.bool,
-  /**
-   * What background color to use
-   */
-  color: PropTypes.string,
   /**
    * How thick should the Divider be?
    */
@@ -38,15 +26,9 @@ Divider.propTypes = {
    * light or dark line?
    */
   strength: PropTypes.oneOf(['light', 'dark']),
-  /**
-   * Divider contents
-   */
-  label: PropTypes.string.isRequired,
 }
 
 Divider.defaultProps = {
-  backgroundColor: null,
-  primary: false,
-  thick: 'medium',
+  thick: null,
   strength: null,
 }
--- src/components/Divider/Divider.stories.js
+++ src/components/Divider/Divider.stories.js
@@ -12,38 +12,27 @@ export default {
 
 const Template = (args) => <Divider {...args} />
 
-export const Primary = Template.bind({})
-Primary.args = {
-  primary: true,
-  label: 'Divider'
-}
-
 export const Thin = Template.bind({})
 Thin.args = {
   thick:'thin',
-  label: 'Divider'
 }
 
 export const Medium = Template.bind({})
 Medium.args = {
   thick: 'medium',
-  label: 'Divider'
 }
 
 export const Thick = Template.bind({})
 Thick.args = {
   thick: 'thick',
-  label: 'Divider'
 }
 
 export const Light = Template.bind({})
 Light.args = {
   strength: 'light',
-  label: 'Divider'
 }
 
 export const Dark = Template.bind({})
 Dark.args = {
   strength: 'dark',
-  label: 'Divider'
 }
