aaa510b00d1765fc8bc008c396c3644bb60856f9
abe menashy <abestbet1@gmail.com>
2020-12-04 12:59:02 -0800

    comment code

--- src/components/Card/Card.stories.tsx
+++ src/components/Card/Card.stories.tsx
@@ -1,123 +1,123 @@
-import React from 'react';
-import  { 
-  Card, 
+import React from 'react'
+import {
+  Card,
   CardImage,
   CardImageContainer,
-  CardTitle, 
-  ListGroup, 
-  ListItem, 
-  CardBody, 
-  CardHeader, 
-  CardFooter, 
-  CardImageOverlay }  from './Card';
+  CardTitle,
+  ListGroup,
+  ListItem,
+  CardBody,
+  CardHeader,
+  CardFooter,
+  CardImageOverlay
+} from './Card'
 
-export default {  
-    component: Card,
-    title: 'Card',
-    argTypes: {
-      align: {
-        control: {
-          type: 'select',
-          options: ['left', 'center', 'right']
-        },
-      }}
+export default {
+  component: Card,
+  title: 'Card',
+  argTypes: {
+    align: {
+      control: {
+        type: 'select',
+        options: ['left', 'center', 'right']
+      }
+    }
   }
-  
+}
 
-
-export const Basic = (args) => <Card {...args}>
-
-<CardImage {...args} />
- <CardBody {...args}>
-  <h3>this is a test</h3>
-  <p>you are the text. you are the text. you are the text</p>
-  <button>Press Me</button>
-</CardBody> 
-</Card>  
+export const Basic = (args) => (
+  <Card {...args}>
+    <CardImage {...args} />
+    <CardBody {...args}>
+      <h3>this is a test</h3>
+      <p>you are the text. you are the text. you are the text</p>
+      <button>Press Me</button>
+    </CardBody>
+  </Card>
+)
 
 Basic.args = {
-   align: ['left', 'center', 'right'],
-    imageSource: 'https://homepages.cae.wisc.edu/~ece533/images/monarch.png',
-    width: 300
+  align: ['left', 'center', 'right'],
+  imageSource: 'https://homepages.cae.wisc.edu/~ece533/images/monarch.png',
+  width: '300'
 }
 
-const Template = (args) => <Card {...args}>
-  <CardImageContainer>
-  <CardImage {...args} />
-  <CardImageOverlay position="bottom-right">
-    <p>hello</p>
-    <p>how are you</p>
-  </CardImageOverlay>
-  </CardImageContainer>
- 
- 
-  <CardHeader />
-  <CardFooter />
-  <ListGroup>
-    <ListItem text="item 1" />
-    <ListItem text="item 2" />
-  </ListGroup>
-</Card>  
+export const HeaderAndFooter = (args) => (
+  <Card {...args}>
+    <CardImageContainer>
+      <CardImage {...args} />
+      <CardImageOverlay position='bottom-right'>
+        <p>hello</p>
+        <p>how are you</p>
+      </CardImageOverlay>
+    </CardImageContainer>
 
-export const HeaderAndFooter = Template.bind({})
+    <CardHeader text='Header' {...args} />
+    <CardFooter text='Footer' {...args} />
+    <ListGroup>
+      <ListItem text='item 1' {...args} />
+      <ListItem text='item 2' {...args} />
+    </ListGroup>
+  </Card>
+)
 
 HeaderAndFooter.args = {
-    title: `I'm the alert`,
-    imageSource: 'https://homepages.cae.wisc.edu/~ece533/images/sails.png',
-    width: 300
+  align: ['left', 'center', 'right'],
+  imageSource: 'https://homepages.cae.wisc.edu/~ece533/images/sails.png',
+  width: '300'
 }
 
+export const ImageOverlay = (args) => (
+  <Card {...args}>
+    <div style={{ position: 'relative' }}>
+      <CardImage {...args} />
+      <CardImageOverlay position='bottom-left'>
+        <h3>hello</h3>
+        <p>over the image</p>
+      </CardImageOverlay>
+    </div>
 
-export const ImageOverlay = (args) => <Card {...args}>
-<div style={{position: 'relative'}}>
-<CardImage {...args} />
-<CardImageOverlay position="bottom-left">
-  <h3>hello</h3>
-  <p>over the image</p>
-</CardImageOverlay>
-</div>
+    <CardBody>
+      <h4>this is a test</h4>
+      <p>you are the text</p>
+      <button>Press Me</button>
+    </CardBody>
 
- <CardBody>
-  <h4>this is a test</h4>
-  <p>you are the text</p>
-  <button>Press Me</button>
-</CardBody> 
-
-<ListGroup>
-  <ListItem text="item 1" />
-  <ListItem text="item 2" />
-</ListGroup>
-</Card>  
+    <ListGroup>
+      <ListItem text='item 1' />
+      <ListItem text='item 2' />
+    </ListGroup>
+  </Card>
+)
 
 ImageOverlay.args = {
-    title: `Hey now hey now2`
+  imageSource: 'https://homepages.cae.wisc.edu/~ece533/images/sails.png',
+  width: '300'
 }
 
-
-export const List = (args) => <Card {...args}>
-
-
-<ListGroup>
-  <ListItem text="item 1" />
-  <ListItem text="item 2" />
-  <ListItem text="item 3" />
-</ListGroup>
-</Card>  
+export const List = (args) => (
+  <Card {...args}>
+    <ListGroup>
+      <ListItem text='item 1' />
+      <ListItem text='item 2' />
+      <ListItem text='item 3' />
+    </ListGroup>
+  </Card>
+)
 
 List.args = {
-    title: `Hey now hey now2`
+  align: ['left', 'center', 'right'],
+  width: '300'
 }
 
-export const ListA = (args) => <Card {...args}>
+export const Combination = (args) => (
+  <Card {...args}>
+    <CardHeader text='Header' />
+    <CardImage {...args} />
+    <CardFooter text='Footer' />
+  </Card>
+)
 
-<CardHeader text="Header"/>
-<CardImage {...args} />
-<CardFooter text="Footer"/>
-</Card>  
-
-ListA.args = {
-    title: `Hey now hey now2`
+Combination.args = {
+  imageSource: 'https://homepages.cae.wisc.edu/~ece533/images/sails.png'
 }
-
-
-
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -10,8 +10,8 @@ interface SimpleCardProps {
 }
 
 interface CompositeCardProps {
-  children?: any;
-  width?: number;
+  children: any;
+  width?: number | string;
   position?: string;
 }
 
@@ -31,73 +31,74 @@ function populateStyleString(props){
 }
 
 // Composite Components (wrap around other elements)
-const Card = ({ width = 300, children }: CompositeCardProps): JSX.Element => (
-  <div className="card" style={{width: `${width}px`}}>{children}</div>
-  )
+const Card = ({ width="300", children }: CompositeCardProps): JSX.Element => {
+  //default width is in pixels, but if string can be appended with
+  //"-rem" or "-pct" for relative widths
+  let widthRule;
+  console.log(width)
+  if (typeof width === 'string'){
+    const [value, type ] = width.split('-');
+    if (type === 'pct'){
+      widthRule = `${value}%`
+    } else if (type === 'rem'){
+      widthRule = `${value}rem`
+    } else {
+      widthRule = `${width}px`
+    }
+  } else {
+    console.log('in else')
+  }
+  return(<div className="card" style={{width: widthRule}}>{children}</div>)
+  }
+const CardImageContainer = ({children }: CompositeCardProps): JSX.Element => (
+  <div className="image-container">{children}</div>)
 
-  const CardImageContainer = ({children }: CompositeCardProps): JSX.Element => (
-    <div className="image-container">{children}</div>
-    )
+const ListGroup = ({ children }: CompositeCardProps): JSX.Element => (
+  <ul className="list-group">{children}</ul>)
 
-  const ListGroup = ({ children }: CompositeCardProps): JSX.Element => (
-    <ul className="list-group">{children}</ul>
-    )
+const CardImageOverlay = ({children, ...props }: CompositeCardProps): JSX.Element => {
+  let styleStr = populateStyleString(props)
+  return(
+    <div className={`image-overlay ${styleStr}`}>{children}</div>)
+}
 
-    const CardImageOverlay = ({children, ...props }: CompositeCardProps): JSX.Element => {
-      let styleStr = populateStyleString(props)
-        return(
-          <div className={`image-overlay ${styleStr}`}>{children}</div>
-        )}
+const CardBody = ({children, ...props }: CompositeCardProps): JSX.Element => {
+  let styleStr = populateStyleString(props)
+  return (
+    <div className={`card-body ${styleStr}`} >{children}</div>
+  )}
 
-  // Regular components (with self-closing tag)
+// Regular components (with self-closing tag)
 
 const CardImage = ({imageSource, ...props}: SimpleCardProps): JSX.Element => {
   let styleStr = populateStyleString(props)
 
   return(<div className={`card-image ${styleStr}`}>
-  <img className="image" src={imageSource} />
- </div>
-)
+    <img className="image" src={imageSource} />
+  </div>)
 }
  
-  
 
 const CardTitle = ({title, ...props }: SimpleCardProps): JSX.Element => {
   let styleStr = populateStyleString(props)
   return(<h2 className={`card-title ${styleStr}`}>{title}</h2>)
-  
 }
   
+const ListItem = ({text, ...props }: SimpleCardProps): JSX.Element => {
+  let styleStr = populateStyleString(props)
+  return(
+    <li className={`list-item ${styleStr}`}>{text}</li>)
+}
 
+const CardHeader = ({text, ...props }: SimpleCardProps): JSX.Element => {
+  let styleStr = populateStyleString(props)
+  return(<div className={`card-header ${styleStr}`} >{text}</div>)
+}
 
-
-  const ListItem = ({text, ...props }: SimpleCardProps): JSX.Element => {
-    let styleStr = populateStyleString(props)
-    return(
-      <li className={`list-item ${styleStr}`}>{text}</li>
-    )}
-
-    const CardBody = ({children, ...props }: SimpleCardProps): JSX.Element => {
-      let styleStr = populateStyleString(props)
-      return (
-        <div className={`card-body ${styleStr}`} >{children}</div>
-      )}
-
-      const CardHeader = ({text, ...props }: SimpleCardProps): JSX.Element => {
-        let styleStr = populateStyleString(props)
-      return(<div className={`card-header ${styleStr}`} >{text}</div>)
-      
-      }
-
-    const CardFooter = ({text, ...props }: SimpleCardProps): JSX.Element => {
-      let styleStr = populateStyleString(props)
-        return(<div className={`card-footer ${styleStr}`} >{text}</div>)
-        
-        }
-
-
-
-
+const CardFooter = ({text, ...props }: SimpleCardProps): JSX.Element => {
+  let styleStr = populateStyleString(props)
+  return(<div className={`card-footer ${styleStr}`} >{text}</div>)
+}
 
 export { 
   Card, 
