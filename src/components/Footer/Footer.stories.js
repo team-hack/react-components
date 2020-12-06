d0c8025096779d0b5db128fc8152af7e86cebeca
James Oquendo <jamesoquendo@Jamess-MacBook-Pro.local>
2020-12-05 22:35:09 -0800

    Add light/dark theme stories and control

--- src/components/Footer/Footer.stories.js
+++ src/components/Footer/Footer.stories.js
@@ -4,11 +4,20 @@ import React from 'react';
 import Footer from './Footer';
 
 export default {
-    title: 'components/Footer',
+    title: 'example/Footer',
     component: Footer,
     decorators: [],
-    parameters: {},
+    backgroundColor: { control: 'color' }
 }
 
-export const Basic = () => <Footer />;
-export const WithProp = () => <Footer prop="value" />;
+const Template = (args) => <Footer {...args} />
+
+export const Light = Template.bind({})
+Light.args = {
+    theme: 'light',
+}
+
+export const Dark = Template.bind({})
+Dark.args = {
+    theme: 'dark',
+}
