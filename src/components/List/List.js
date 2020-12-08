32ca67b5712831581a635723988f28fbfb32bfa3
Shannan Bunch <shannan.bunch@tvgla.com>
2020-12-08 14:51:17 -0800

    init list component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,3 +1,29 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+{/* <List listItems={[]} isOrdered = {false}>  */}
+
+export const List = (props) => {
+
+    if (isOrdered) {
+      return (
+        <ol>
+        {props.listItems.map((listItem, i) => {
+          return (
+            <li>{listItem}</li>
+          )
+        }) }
+      </ol>
+      ) 
+    }
+      return (
+      <ul>
+      {props.listItems.map((listItem, i) => {
+        return (
+          <li>{listItem}</li>
+        )
+      }) }
+    </ul>
+
+    )
+  } 
