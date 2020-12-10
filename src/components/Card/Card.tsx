f339d7fefeaebce0c77724770eeb372d2c3eac1d
abe menashy <abestbet1@gmail.com>
2020-12-09 22:05:28 -0800

    tests for Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,46 +1,48 @@
 import React from 'react'
-import './card.css';
+import './card.css'
 
 interface SimpleCardProps {
-  title?: string;
-  imageSource?: string;
-  bold?: boolean;
-  text?: string;
-  align?: string;
+  title?: string
+  imageSource?: string
+  bold?: boolean
+  text?: string
+  align?: string
 }
 
 interface CompositeCardProps {
-  children: any;
-  width?: number | string;
-  position?: string;
+  children: any
+  width?: number | string
+  position?: string
+  align?: string
 }
 
-
-
-function populateStyleString(props){
-  let str = '';
-  Object.keys(props).map(item => {
-    if(item === 'align' || item=== 'position'){
+function populateStyleString(props) {
+  let str = ''
+  Object.keys(props).map((item) => {
+    if (item === 'children') {
+      return
+    }
+    if (item === 'align' || item === 'position') {
       str += `${props[item]} `
     } else {
       str += `${item} `
     }
   })
-   
-  return str;
+
+  return str
 }
 
 // Composite Components (wrap around other elements)
-const Card = ({ width="300", children }: CompositeCardProps): JSX.Element => {
+const Card = ({ width = '300', children }: CompositeCardProps): JSX.Element => {
   //default width is in pixels, but if string can be appended with
   //"-rem" or "-pct" for relative widths
-  let widthRule;
+  let widthRule
   console.log(width)
-  if (typeof width === 'string'){
-    const [value, type ] = width.split('-');
-    if (type === 'pct'){
+  if (typeof width === 'string') {
+    const [value, type] = width.split('-')
+    if (type === 'pct') {
       widthRule = `${value}%`
-    } else if (type === 'rem'){
+    } else if (type === 'rem') {
       widthRule = `${value}rem`
     } else {
       widthRule = `${width}px`
@@ -48,67 +50,74 @@ const Card = ({ width="300", children }: CompositeCardProps): JSX.Element => {
   } else {
     console.log('in else')
   }
-  return(<div className="card" style={{width: widthRule}}>{children}</div>)
-  }
-const CardImageContainer = ({children }: CompositeCardProps): JSX.Element => (
-  <div className="image-container">{children}</div>)
+  return (
+    <div className='card' style={{ width: widthRule }}>
+      {children}
+    </div>
+  )
+}
+const CardImageContainer = ({ children }: CompositeCardProps): JSX.Element => (
+  <div className='image-container'>{children}</div>
+)
 
 const ListGroup = ({ children }: CompositeCardProps): JSX.Element => (
-  <ul className="list-group">{children}</ul>)
+  <ul className='list-group'>{children}</ul>
+)
 
-const CardImageOverlay = ({children, ...props }: CompositeCardProps): JSX.Element => {
+const CardImageOverlay = ({
+  children,
+  ...props
+}: CompositeCardProps): JSX.Element => {
   let styleStr = populateStyleString(props)
-  return(
-    <div className={`image-overlay ${styleStr}`}>{children}</div>)
+  return <div className={`image-overlay ${styleStr}`}>{children}</div>
 }
 
-const CardBody = ({children, ...props }: CompositeCardProps): JSX.Element => {
+const CardBody = ({ children, ...props }: CompositeCardProps): JSX.Element => {
   let styleStr = populateStyleString(props)
-  return (
-    <div className={`card-body ${styleStr}`} >{children}</div>
-  )}
+  return <div className={`card-body ${styleStr}`}>{children}</div>
+}
 
 // Regular components (with self-closing tag)
 
-const CardImage = ({imageSource, ...props}: SimpleCardProps): JSX.Element => {
+const CardImage = ({ imageSource, ...props }: SimpleCardProps): JSX.Element => {
   let styleStr = populateStyleString(props)
-
-  return(<div className={`card-image ${styleStr}`}>
-    <img className="image" src={imageSource} />
-  </div>)
-}
- 
-
-const CardTitle = ({title, ...props }: SimpleCardProps): JSX.Element => {
-  let styleStr = populateStyleString(props)
-  return(<h2 className={`card-title ${styleStr}`}>{title}</h2>)
-}
-  
-const ListItem = ({text, ...props }: SimpleCardProps): JSX.Element => {
-  let styleStr = populateStyleString(props)
-  return(
-    <li className={`list-item ${styleStr}`}>{text}</li>)
+  const srcProp = imageSource ? imageSource : ''
+  return (
+    <div className={`card-image ${styleStr}`}>
+      <img className='image' src={srcProp} />
+    </div>
+  )
 }
 
-const CardHeader = ({text, ...props }: SimpleCardProps): JSX.Element => {
+const CardTitle = ({ title, ...props }: SimpleCardProps): JSX.Element => {
   let styleStr = populateStyleString(props)
-  return(<div className={`card-header ${styleStr}`} >{text}</div>)
+  return <h2 className={`card-title ${styleStr}`}>{title}</h2>
 }
 
-const CardFooter = ({text, ...props }: SimpleCardProps): JSX.Element => {
+const ListItem = ({ text, ...props }: SimpleCardProps): JSX.Element => {
   let styleStr = populateStyleString(props)
-  return(<div className={`card-footer ${styleStr}`} >{text}</div>)
+  return <li className={`list-item ${styleStr}`}>{text}</li>
 }
 
-export { 
-  Card, 
+const CardHeader = ({ text, ...props }: SimpleCardProps): JSX.Element => {
+  let styleStr = populateStyleString(props)
+  return <div className={`card-header ${styleStr}`}>{text}</div>
+}
+
+const CardFooter = ({ text, ...props }: SimpleCardProps): JSX.Element => {
+  let styleStr = populateStyleString(props)
+  return <div className={`card-footer ${styleStr}`}>{text}</div>
+}
+
+export {
+  Card,
   CardImageContainer,
-  CardImage, 
-  CardTitle, 
-  ListGroup, 
-  ListItem, 
-  CardBody, 
-  CardHeader, 
-  CardFooter, 
-  CardImageOverlay }
-
+  CardImage,
+  CardTitle,
+  ListGroup,
+  ListItem,
+  CardBody,
+  CardHeader,
+  CardFooter,
+  CardImageOverlay
+}
