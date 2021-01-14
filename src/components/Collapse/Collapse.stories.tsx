a971e94bc8089f5167ad7ed6501e617dc43419ee
abe menashy <abestbet1@gmail.com>
2021-01-14 11:46:05 -0800

    handle accordian functionality

--- src/components/Collapse/Collapse.stories.tsx
+++ src/components/Collapse/Collapse.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Collapse } from './Collapse';
+import { Card } from '../Card/Card';
 
 export default {
   component: Collapse,
@@ -17,24 +18,26 @@ export default {
 export const Basic = (args) => (
   <div>
     <Collapse {...args}>
-      <button
-        className='btn btn-primary'
-        type='button'
-        data-toggle='collapse'
-        data-target='#collapseExample'
-        aria-expanded='false'
-        aria-controls='collapseExample'
-      >
-        Button with data-target
-      </button>
+      <Card>
+        <button
+          className='btn btn-primary'
+          type='button'
+          data-toggle='collapse'
+          data-target='#collapseExample'
+          aria-expanded='false'
+          aria-controls='collapseExample'
+        >
+          Button with data-target
+        </button>
 
-      <div className='collapse' id='collapseExample'>
-        <div className='card card-body'>
-          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
-          terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
-          labore wes anderson cred nesciunt sapiente ea proident.
+        <div className='collapse' id='collapseExample'>
+          <div className='card card-body'>
+            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+            terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+            labore wes anderson cred nesciunt sapiente ea proident.
+          </div>
         </div>
-      </div>
+      </Card>
     </Collapse>
   </div>
 );
@@ -44,3 +47,62 @@ Basic.args = {
   imageSource: 'https://homepages.cae.wisc.edu/~ece533/images/monarch.png',
   width: '300'
 };
+
+export const Accordian = (args) => (
+  <Card>
+    <Collapse {...args}>
+      <div
+        style={{
+          backgroundColor: 'rgb(245,245,245)',
+          borderBottom: '1px solid rgb(211,211,211)',
+          padding: '20px'
+        }}
+        data-toggle='collapse'
+        data-target='#collapseExample1'
+        aria-expanded='false'
+        aria-controls='collapseExample'
+      >
+        Button with data-target
+      </div>
+
+      <div className='collapse' id='collapseExample1'>
+        <div
+          style={{
+            padding: '10px'
+          }}
+        >
+          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+          terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+          labore wes anderson cred nesciunt sapiente ea proident.
+        </div>
+      </div>
+    </Collapse>
+    <Collapse {...args}>
+      <div
+        style={{
+          backgroundColor: 'rgb(245,245,245)',
+          borderBottom: '1px solid rgb(211,211,211)',
+          padding: '20px'
+        }}
+        data-toggle='collapse'
+        data-target='#collapseExample2'
+        aria-expanded='false'
+        aria-controls='collapseExample'
+      >
+        Button with data-target
+      </div>
+
+      <div className='collapse' id='collapseExample2'>
+        <div
+          style={{
+            padding: '10px'
+          }}
+        >
+          Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus
+          terry richardson ad squid. Nihil anim keffiyeh helvetica, craft beer
+          labore wes anderson cred nesciunt sapiente ea proident.
+        </div>
+      </div>
+    </Collapse>
+  </Card>
+);
--- src/components/Collapse/Collapse.tsx
+++ src/components/Collapse/Collapse.tsx
@@ -7,41 +7,44 @@ interface CollapseProps {
 
 // Composite Components (wrap around other elements)
 const Collapse = ({ children }: CollapseProps): JSX.Element => {
-  console.log('CHILDREN', children);
-  const [collapse, setCollapse] = useState(true);
+  const [target, setTarget] = useState('');
   let dataTarget = '';
-  return (
-    <div>
-      {React.Children.map(children, (child, i) => {
-        console.log(child);
-        if (child.props['data-toggle'] === 'collapse') {
-          dataTarget = child.props['data-target'].substring(1);
-          const childProps = child.props;
-          return React.cloneElement(child, {
-            ...child.props,
-            onClick: (n) => {
-              console.log('has been clicked');
-              setCollapse(!collapse);
-            }
-          });
+
+  const childElements = React.Children.map(children, (child, i) => {
+    console.log(child);
+    if (child.props['data-toggle'] === 'collapse') {
+      dataTarget = child.props['data-target'].substring(1);
+
+      return React.cloneElement(child, {
+        ...child.props,
+
+        onClick: (e) => {
+          e.stopPropagation();
+          let panel = document.getElementById(target);
+
+          if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+          } else {
+            panel.style.maxHeight = panel.scrollHeight + 'px';
+          }
         }
-        if (child.props.id === dataTarget) {
-          const childProps = child.props;
-          //   const className = classNames(
-          //     child.props.className,
-          //     {...otherClassses}
-          // );
-          let className = collapse ? 'collapse' : 'open';
-          console.log('second', child.props.className);
-          return React.cloneElement(child, {
-            ...child.props,
-            className
-          });
-        }
-        return null;
-      })}
-    </div>
-  );
+      });
+    }
+    if (child.props.id === dataTarget) {
+      console.log(child.props.id, document.querySelector(`#${child.props.id}`));
+
+      if (target === '') {
+        setTarget(child.props.id);
+      }
+
+      return React.cloneElement(child, {
+        ...child.props
+      });
+    }
+    return null;
+  });
+
+  return <div>{childElements}</div>;
 };
 
 export { Collapse };
