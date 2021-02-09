31f76c65f2e37cb9d693014d0d2e40031594828b
abe menashy <abestbet1@gmail.com>
2021-02-09 14:41:12 -0800

    tests for jumbotron

--- src/components/Jumbotron/Jumbotron.stories.tsx
+++ src/components/Jumbotron/Jumbotron.stories.tsx
@@ -16,14 +16,17 @@ export default {
 
 export const Basic = (args) => {
   return (
-    <Jumbotron {...args}>
-      <p>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi illo
-        sunt incidunt iusto, fuga ea aspernatur temporibus consequatur
-        distinctio quibusdam? Ea laborum numquam aspernatur asperiores nihil
-        deleniti repellendus eos quis?
-      </p>
-    </Jumbotron>
+    <div style={{ marginTop: '30px' }}>
+      <Jumbotron {...args}>
+        <h2>Lorem ipsum dolor sit</h2>
+        <p>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi illo
+          sunt incidunt iusto, fuga ea aspernatur temporibus consequatur
+          distinctio quibusdam? Ea laborum numquam aspernatur asperiores nihil
+          deleniti repellendus eos quis?
+        </p>
+      </Jumbotron>
+    </div>
   );
 };
 
